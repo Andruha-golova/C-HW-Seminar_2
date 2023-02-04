ef3ccf5ed7b22cd32206c916156a11827cfb5044
Andrey <andrey.ra4kov1988@gmail.com>
2023-02-04 22:34:51 +0300

    avgnumber

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("enter three-digit number:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int AverageNumber = (number % 100) / 10;
+
+System.Console.WriteLine($"average number {AverageNumber}");
