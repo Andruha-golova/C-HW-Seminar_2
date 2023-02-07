fcbb877891d0ee45f3cb4b9d8150b575596214a0
Andrey <andrey.ra4kov1988@gmail.com>
2023-02-07 22:32:54 +0300

    Task13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿System.Console.WriteLine("enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    System.Console.WriteLine("the third digit is missing");
+}
+else
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    System.Console.WriteLine(number%10);
+}
